d5110b6f5e2c203ed4d574dbbe5f7343b4e87893
Dev Dixit <devdixitsocial@gmail.com>
2025-04-28 20:38:36 +0530

    create auth page, setup file structure

--- app.js
+++ app.js
@@ -23,6 +23,14 @@ app.get('/', (req, res) => {
   res.render('welcome')
 });
 
+app.get('/signup', (req, res) => {
+  res.render('register')
+});
+
+app.get('/login', (req, res) => {
+  res.render('login')
+});
+
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
 });
